1f286ad5ae97439f04824b203dcdd68635c51ec1
imayk1002 <132862541+imayk1002@users.noreply.github.com>
2024-01-16 11:06:57 +0800

    Add files via upload

--- MyWebsite/index.js
+++ MyWebsite/index.js
@@ -1,67 +1,67 @@
 
-function login() {
+// function login() {
 
-    var acc = [
-        {
-            username: "imayk",
-            password: "imayk"
-        },
-        {
-            username: "imayk1",
-            password: "imayk1"
-        },
-        {
-            username: "imayk2",
-            password: "imayk2"
-        }
-    ];
+//     var acc = [
+//         {
+//             username: "imayk",
+//             password: "imayk"
+//         },
+//         {
+//             username: "imayk1",
+//             password: "imayk1"
+//         },
+//         {
+//             username: "imayk2",
+//             password: "imayk2"
+//         }
+//     ];
 
-    var username = document.getElementById("txtEmail").value;
-    var password = document.getElementById("txtPass").value;
-    var found = false;
+//     var username = document.getElementById("txtEmail").value;
+//     var password = document.getElementById("txtPass").value;
+//     var found = false;
 
-    for (var i = 0; i < acc.length; i++) {
-        if (username == acc[i].username && password == acc[i].password) {
-            alert("Login Connected");
-            window.location.href = "index.php";
-            found = true;
-            break;
-        }
-    }
+//     for (var i = 0; i < acc.length; i++) {
+//         if (username == acc[i].username && password == acc[i].password) {
+//             alert("Login Connected");
+//             window.location.href = "index.php";
+//             found = true;
+//             break;
+//         }
+//     }
 
-    if (!found) {
-        alert("Access Denied");
-        document.getElementById("error").style.display = "block";
+//     if (!found) {
+//         alert("Access Denied");
+//         document.getElementById("error").style.display = "block";
 
-        for (var i = 0; i < acc.length; i++) {
-            if (username != acc[i].username) {
-                document.getElementById("txtEmail").style.borderBottomColor = "red";
-            } else if (password != acc[i].password) {
-                document.getElementById("txtPass").style.borderBottomColor = "red";
-            } else {
-                document.getElementById("txtEmail").style.borderBottomColor = "red";
-                document.getElementById("txtPass").style.borderBottomColor = "red";
-            }
-        }
-    }
-}
+//         for (var i = 0; i < acc.length; i++) {
+//             if (username != acc[i].username) {
+//                 document.getElementById("txtEmail").style.borderBottomColor = "red";
+//             } else if (password != acc[i].password) {
+//                 document.getElementById("txtPass").style.borderBottomColor = "red";
+//             } else {
+//                 document.getElementById("txtEmail").style.borderBottomColor = "red";
+//                 document.getElementById("txtPass").style.borderBottomColor = "red";
+//             }
+//         }
+//     }
+// }
 
 
-function signup() {
+// function signup() {
 
-    const password = document.getElementById("txtPass").value;
+//     const password = document.getElementById("txtPass").value;
 
-    var sChar = /[!@#$%^&*(),.?":{}|<>]/;
-    var num = /\d/;
+//     var sChar = /[!@#$%^&*(),.?":{}|<>]/;
+//     var num = /\d/;
 
-    if (password.length < 8) {
-        alert("Password must be at least 8 characters long.");
-    } else if (!password.match(sChar)) {
-        alert("Password must contain at least one special character.");
-    } else if (!password.match(num)) {
-        alert("Password must contain at least one numeric digit.");
-    } else {
-        alert("Sign up successful");
-        window.location.href = "login.php";
-    }
-}
+//     if (password.length < 8) {
+//         alert("Password must be at least 8 characters long.");
+//     } else if (!password.match(sChar)) {
+//         alert("Password must contain at least one special character.");
+//     } else if (!password.match(num)) {
+//         alert("Password must contain at least one numeric digit.");
+//     } else {
+//         alert("Sign up successful");
+//         window.location.href = "login.php";
+//     }
+// }
--- MyWebsite/login.php
+++ MyWebsite/login.php
@@ -1,61 +1,40 @@
+
 <?php
-  session_start();
+session_start();
+include("db.php");
 
-  include("db.php");
-  
-  if($_SERVER['REQUEST_METHOD'] == "POST")
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  if(!empty($email) && !empty($password) && !is_numeric($email))
   {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    $query = "select * from form where email = '$email' limit1";
+    $result = mysql_query($con,$query);
 
-    if(!empty($email) && !empty($password) && !is_numeric($email))
+    if($result)
     {
-      $squery = "select from from where email = '$gmail' limit 1";
-      $result = mysqli_query($con, $query);
-
-      if($result)
+      if($result && mysqli_num_rows($result) > 0)
       {
-        if($result && mysql_num_rows($result) > 0)
+        $user_data = mysql_fetch_assoc($result);
+
+        if($user_data['pass'] == $password)
         {
-          $user_data = mysqli_fetch_assoc($result);
-
-          
-
+          header("location: success.php");
+          die;
         }
       }
-      echo "<script type='text/javacript'> alert('wrong username or password') </script";
     }
-    else{
-      echo "<script type='text/javacript'> alert('wrong username or password') </script";
-   }
- }
-  
+    echo "<script> alert('Wrong Password'); </script>";
+  }
+  else{
+    "<script> alert('Wrong Password'); </script>";
+
+  }
+}
 ?>
-<?php
-require 'success.php';
-if(isset($_POST["submit"])){
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $email = $_POST["email"];
-    $result = mysql_query($conn, "SELECT * FROM tb_user WHERE username = '$username' OR email = '$email ='$usernameemail'");
-    $row = mysqli_fetch_assoc($result);
-    if(mysql_num_rows($result) > 0){
-        if($password == $row["password"]){
-            $_SESSION["login"] = true;
-            $_SESSION["id"] = $row["id"];
-            header("Location: error.php");
-        }
-        else{
-            echo
-            "<script> alert('Wrong Password'); </script>";
-        }
-    }
-     else{
-        echo
-        "<script> alert('User Not Registered'); </script>";
-     }
-    }
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -65,31 +44,21 @@ if(isset($_POST["submit"])){
     <title>BKDR LOGIN</title>
 </head>
 <body>
-    
-    
-    
-    <h1>HELLO WELCOME!</h1>
+
     
     <div class="container">
-    
-        <form action="post">
-            <h1>Login</h1>
-            <span id="error">Invalid username or password!</span>
-            <div class="form">        
-                <input id="txtEmail" type="email" placeholder="Username">
+        <form method="POST" action="login.php">
+            <h1> BKDR Login</h1>
+            <span id="error"></span>
+            <div class="input-group">        
+                <input id="txtEmail" type="email" placeholder="Email">
             </div>
-            <div class="form">
+            <div class="input-group">
                 <input id="txtPass" type="password" placeholder="Password">
             </div>
-            <div class="remember">
-                <input type="checkbox" placeholder="checkbox">
-                <label>remember me</label>
-            </div>
-            <div class="forgot-pass">       
-                <a href="#">Forgot password?</a>
-            </div>
+
             <div class="form">
-                <button id="btnLogin" type="button" onclick="login()">Sign in</button>
+                <button id="btnLogin" type="submit">Sign in</button>
             </div>  
             <div class="new">
                 <a href="signup.php">Sign Up</a>
@@ -99,8 +68,7 @@ if(isset($_POST["submit"])){
             </div>
         </form>
     </div>
-
-    <script src="index.js "></script>
-
+    <!-- <script src="index.js "></script> -->
+    
 </body>
 </html>
